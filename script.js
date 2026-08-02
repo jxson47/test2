32c278eb7b07b605617548b5389a5cc10e61bb0c
jxson47 <jasonlem47533@gmail.com>
2026-08-03 00:25:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function register(){
+
+alert("Registrierung wird später mit Firebase verbunden.");
+
+}
+
+
+
+function login(){
+
+let email=document.getElementById("email").value;
+
+
+if(email==""){
+
+alert("Bitte E-Mail eingeben");
+
+}
+
+else {
+
+alert("Login wird später aktiviert.");
+
+}
+
+
+}
